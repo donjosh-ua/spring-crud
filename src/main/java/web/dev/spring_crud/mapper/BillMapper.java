febb3db4f36d8ef26180d19f5545fc94fa08f959
Joshua Cueva <joshua.cueva@ucuenca.edu.ec>
2025-06-05 08:39:29 -0500

    Mappers creation

--- src/main/java/web/dev/spring_crud/mapper/BillMapper.java
+++ src/main/java/web/dev/spring_crud/mapper/BillMapper.java
@@ -1,9 +1,26 @@
 package web.dev.spring_crud.mapper;
 
+import java.util.Set;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants.ComponentModel;
 
+import web.dev.spring_crud.domain.dto.BillDTO;
+import web.dev.spring_crud.domain.model.Bill;
+
 @Mapper(componentModel = ComponentModel.SPRING)
 public interface BillMapper {
+
+    // map dto to entity
+    Bill toEntity(BillDTO billDTO);
+
+    // map entity to dto
+    BillDTO toDTO(Bill bill);
+
+    // map set of dto to set of entity
+    Set<Bill> toEntitySet(Set<BillDTO> billDTOs);
+
+    // map set of entity to set of dto
+    Set<BillDTO> toDTOSet(Set<Bill> bills);
 
 }
--- src/main/java/web/dev/spring_crud/mapper/PersonMapper.java
+++ src/main/java/web/dev/spring_crud/mapper/PersonMapper.java
@@ -1,9 +1,26 @@
 package web.dev.spring_crud.mapper;
 
+import java.util.Set;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants.ComponentModel;
 
+import web.dev.spring_crud.domain.dto.PersonDTO;
+import web.dev.spring_crud.domain.model.Person;
+
 @Mapper(componentModel = ComponentModel.SPRING)
 public interface PersonMapper {
+
+    // map dto to entity
+    Person toEntity(PersonDTO personDTO);
+
+    // map entity to dto
+    PersonDTO toDTO(Person person);
+
+    // map set of dto to set of entity
+    Set<Person> toEntitySet(Set<PersonDTO> personDTOs);
+
+    // map set of entity to set of dto
+    Set<PersonDTO> toDTOSet(Set<Person> persons);
 
 }
