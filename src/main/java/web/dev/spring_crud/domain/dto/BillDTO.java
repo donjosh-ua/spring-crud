7f30471d8fb621539e104aa19ae8498814361af7
Joshua Cueva <joshua.cueva@ucuenca.edu.ec>
2025-06-05 08:36:24 -0500

    Dtos creation

--- src/main/java/web/dev/spring_crud/domain/dto/BillDTO.java
+++ src/main/java/web/dev/spring_crud/domain/dto/BillDTO.java
@@ -1,5 +1,11 @@
 package web.dev.spring_crud.domain.dto;
 
+import java.util.Date;
+
 public class BillDTO {
+
+    public Float amount;
+    public Date date;
+    public String topic;
 
 }
--- src/main/java/web/dev/spring_crud/domain/dto/PersonDTO.java
+++ src/main/java/web/dev/spring_crud/domain/dto/PersonDTO.java
@@ -1,5 +1,12 @@
 package web.dev.spring_crud.domain.dto;
 
+import java.util.Set;
+
 public class PersonDTO {
+
+    public Integer id;
+    public String name;
+    public String email;
+    public Set<BillDTO> bills;
 
 }
